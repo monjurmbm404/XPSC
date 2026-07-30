aeb417fa16c844b9e25a7589ab2f3c78bc808311
Md Monjur Bakth Mazumder <md.monjurmbm2001@gmail.com>
2026-07-30 07:19:38 +0600

    update

---
+++ 18 Bit Manipulation - 2/Bitmasks.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    for (int mask = 0; mask < (1 << n); mask++)
+    {
+        cout << mask << " -> ";
+        for (int k = n - 1; k >= 0; k--)
+        {
+            if ((mask >> k) & 1)
+                cout << 1;
+            else
+                cout << 0;
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
