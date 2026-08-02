7b76ad83158a9270d22f8f097ca6a6579f6724f3
Md Monjur Bakth Mazumder <md.monjurmbm2001@gmail.com>
2026-08-02 20:07:29 +0600

    update

--- 18 Bit Manipulation - 2/B_Rock_and_Lever.cpp
+++ 18 Bit Manipulation - 2/B_Rock_and_Lever.cpp
@@ -5,7 +5,20 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, x;
+        cin >> n;
+        vector<int> cnt(30);
+        for (int i = 0; i < n; i++)
+            cin >> x, cnt[__lg(x)]++;
+        long long ans = 0;
+        for (int i = 0; i < 30; i++)
+            ans += (1LL * cnt[i] * (cnt[i] - 1)) / 2;
+        cout << ans << "\n";
+    }
 
     return 0;
 }
